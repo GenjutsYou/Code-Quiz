dc22f8d5cd0b4dafea7ed15f610c91c81cdbfa68
GenjutsYou <arunmundackal7@gmail.com>
2023-04-13 16:46:39 -0700

    updated structure

--- script.js
+++ script.js
@@ -14,57 +14,57 @@ var saveButtonEl = document.getElementById("save-button");
 
 // Quiz Questions
 var questions = [
-    {
-      question: "What does CSS stand for?",
-      choices: ["Cascading Style Sheets", "Central Style System", "Creative Styling Solutions", "Computer Styling Standards"],
-      answer: "Cascading Style Sheets"
+  {
+    question: "What does CSS stand for?",
+    choices: ["a)Cascading Style Sheets", "b)Central Style System", "c)Creative Styling Solutions", "d)Computer Styling Standards"],
+    answer: "a)Cascading Style Sheets"
+  },
+  {
+    question: "What is the purpose of JavaScript?",
+    choices: ["a)To add styling to web pages", "b)To create server-side databases", "c)To add interactivity to web pages", "d)To design user interfaces"],
+    answer: "c)To add interactivity to web pages"
+  },
+  {
+    question: "What is the purpose of the 'console.log()' function in JavaScript?",
+    choices: ["a)To display messages on the web page", "b)To execute a function", "c)To send data to the server", "d)To print output to the console"],
+    answer: "d)To print output to the console"
     },
     {
-      question: "What is the purpose of JavaScript?",
-      choices: ["To add styling to web pages", "To create server-side databases", "To add interactivity to web pages", "To design user interfaces"],
-      answer: "To add interactivity to web pages"
+    question: "What does the 'DOCTYPE' declaration in HTML do?",
+    choices: ["a)Specifies the character encoding of the document", "b)Defines the version of HTML used", "c)Indicates the type of document being used", "d)Specifies the title of the web page"],
+    answer: "c)Indicates the type of document being used"
     },
     {
-      question: "What is the correct syntax for a JavaScript comment?",
-      choices: ["// This is a comment", "<!-- This is a comment -->", "/* This is a comment */", "## This is a comment"],
-      answer: "// This is a comment"
+    question: "What is the correct syntax for creating a new variable in JavaScript?",
+    choices: ["a)var newVar = 10;", "b)let newVar = 10;", "c)const newVar = 10;", "d)int newVar = 10;"],
+    answer: "b)let newVar = 10;"
     },
     {
-      question: "What is an array in JavaScript?",
-      choices: ["A type of loop", "A function", "A variable", "A collection of values"],
-      answer: "A collection of values"
+    question: "What does the 'margin' property in CSS define?",
+    choices: ["a)The space inside an element's border", "b)The space outside an element's border", "c)The width of an element", "d)The height of an element"],
+    answer: "b)The space outside an element's border"
     },
     {
-      question: "Which operator is used for strict equality comparison in JavaScript?",
-      choices: ["==", "===", "!==", "!="],
-      answer: "==="
+    question: "What does the 'addEventListener()' method in JavaScript do?",
+    choices: ["a)Changes the background color of an element", "b)Adds an event handler function to an element", "c)Modifies the content of an element", "d)Creates a new HTML element"],
+    answer: "b)Adds an event handler function to an element"
     },
     {
-      question: "What is the purpose of the 'for' loop in JavaScript?",
-      choices: ["To define a function", "To add styling to web pages", "To create server-side databases", "To repeat a block of code"],
-      answer: "To repeat a block of code"
+    question: "What is the purpose of the 'box-sizing' property in CSS?",
+    choices: ["a)Defines the layout of a web page", "b)Specifies the size of an image", "c)Controls the box model of an element", "d)Sets the color of a text"],
+    answer: "c)Controls the box model of an element"
     },
     {
-      question: "What is the correct syntax for a function declaration in JavaScript?",
-      choices: ["function myFunction()", "myFunction = function()", "myFunction()", "myFunction: function()"],
-      answer: "function myFunction()"
+    question: "What is the correct way to write a comment in JavaScript?",
+    choices: ["a)// This is a comment", "b)<!-- This is a comment -->", "c)/* This is a comment */", "d)<!--! This is a comment -->"],
+    answer: "a)// This is a comment"
     },
     {
-      question: "What is the purpose of 'localStorage' in JavaScript?",
-      choices: ["To store variables on the server", "To store variables on the client", "To create animations", "To manipulate images"],
-      answer: "To store variables on the client"
+    question: "What is the purpose of the 'text-align' property in CSS?",
+    choices: ["a)Sets the background color of an element", "b)Changes the font size of an element", "c)Defines the alignment of text within an element", "d)Controls the spacing between lines of text"],
+    answer: "c)Defines the alignment of text within an element"
     },
-    {
-      question: "What does the 'getElementById()' method do in JavaScript?",
-      choices: ["Changes the color of an element", "Selects an element by its class name", "Selects an element by its tag name", "Selects an element by its ID"],
-      answer: "Selects an element by its ID"
-    },
-    {
-      question: "What is the purpose of 'JSON.parse()' in JavaScript?",
-      choices: ["To convert a JSON string into a JavaScript object", "To convert a JavaScript object into a JSON string", "To create a new JSON object", "To delete a property from a JSON object"],
-      answer: "To convert a JSON string into a JavaScript object"
-    }
-  ];  
+];  
 
 // Event Listeners
 startButtonEl.addEventListener("click", startQuiz);
